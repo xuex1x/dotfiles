6cbb9e21fd5e0ad8f989801d02c1d3fcc2b06b08
xuex1x <96759958+xuex1x@users.noreply.github.com>
2025-08-21 15:14:34 +0800

    Update setup.sh

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -44,15 +44,15 @@ function install_packages() {
     moreutils
     # openssh-server
     p7zip-full
-    p7zip-rar
+    # p7zip-rar
     perl
     poppler-utils
     python3
     python3-pip
     pigz
-    software-properties-common
+    # software-properties-common
     tree
-    unrar
+    # unrar
     unzip
     wget
     x11-utils
@@ -72,7 +72,7 @@ function install_packages() {
     fi
 
     sudo apt-get update
-    #sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::options::=--force-confdef -o DPkg::options::=--force-confold upgrade -y'
+    sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::options::=--force-confdef -o DPkg::options::=--force-confold upgrade -y'
     sudo apt-get install -y "${packages[@]}"
     sudo apt-get autoremove -y
     sudo apt-get autoclean
